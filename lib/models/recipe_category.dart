90743e0c58a6df0c0299d9b19ea72c45ef42c6a7
alemdisso <rodrigo.aocubo@gmail.com>
2025-05-23 19:12:02 -0300

    Add RecipeCategory enum for categorizing recipes

---
+++ lib/models/recipe_category.dart
@@ -0,0 +1,47 @@
+enum RecipeCategory {
+  mainDishes('main_dishes'),
+  sideDishes('side_dishes'),
+  sandwiches('sandwiches'),
+  completeMeals('complete_meals'),
+  breakfastItems('breakfast_items'),
+  desserts('desserts'),
+  soupsStews('soups_stews'),
+  salads('salads'),
+  snacks('snacks'),
+  uncategorized('uncategorized');
+
+  final String value;
+  const RecipeCategory(this.value);
+
+  static RecipeCategory fromString(String value) {
+    return RecipeCategory.values.firstWhere(
+      (type) => type.value == value,
+      orElse: () => RecipeCategory.uncategorized,
+    );
+  }
+
+  String get displayName {
+    switch (this) {
+      case RecipeCategory.mainDishes:
+        return 'Main dishes';
+      case RecipeCategory.sideDishes:
+        return 'Side dishes';
+      case RecipeCategory.sandwiches:
+        return 'Sandwiches';
+      case RecipeCategory.completeMeals:
+        return 'Complete meals';
+      case RecipeCategory.breakfastItems:
+        return 'Breakfast items';
+      case RecipeCategory.desserts:
+        return 'Desserts';
+      case RecipeCategory.soupsStews:
+        return 'Soups/stews';
+      case RecipeCategory.salads:
+        return 'Salads';
+      case RecipeCategory.snacks:
+        return 'Snacks';
+      case RecipeCategory.uncategorized:
+        return 'Uncategorized';
+    }
+  }
+}
